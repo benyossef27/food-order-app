88e4d70203d258b466b8e8c37b28a0788d5e6dcf
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2022-04-25 16:42:34 +0300

    add items to cart

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,30 @@
+import { useRef, useState } from "react";
 import Input from "./Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    const enturedAmountNumber = +enteredAmount;
+    if (
+      enteredAmount.trim().length === 0 ||
+      enturedAmountNumber < 1 ||
+      enturedAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+    props.onAddToCart(enturedAmountNumber);
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       {/* the input props are default props for input elements. wich is why we use ... to spread them in "Input.js"*/}
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
@@ -17,6 +36,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount</p>}
     </form>
   );
 };
